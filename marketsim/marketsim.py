f96b8af4b9c0cf31d41bacd6773161613fbecbed
Abel <aguilarabel165@gmail.com>
2023-02-23 13:19:15 -0800

    working port

--- marketsim/marketsim.py
+++ marketsim/marketsim.py
@@ -26,11 +26,10 @@ GT User ID: aaguilar61
 GT ID: 903861561 		  	   		  		 			  		 			     			  	 
 """  		  	   		  		 			  		 			     			  	 
   		  	   		  		 			  		 			     			  	 
-import datetime as dt  		  	   		  		 			  		 			     			  	 
-import os  		  	   		  		 			  		 			     			  	 
+import datetime as dt  	   		  		 			  		 			     			  	 
   		  	   		  		 			  		 			     			  	 
 import numpy as np  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
+import math as m 	   		  		 			  		 			     			  	 
 import pandas as pd  		  	   		  		 			  		 			     			  	 
 from util import get_data, plot_data  		  	   		  		 			  		 			     			  	 
 
@@ -39,10 +38,47 @@ def author():
     :return: The GT username of the student  		  	   		  		 			  		 			     			  	 
     :rtype: str  		  	   		  		 			  		 			     			  	 
     """  		  	   		  		 			  		 			     			  	 
-    return "aaguilar61"  # Change this to your user ID  
+    return "aaguilar61"
+
+def findPortStats(portval):
+    sf = 252
+    rfr = 0.0
+
+    port_val = portval		  	   		  		 			  		 			     			  	 
+
+    daily_returns = port_val.copy()
+    daily_returns = (port_val / port_val.shift(1)) - 1           
+
+    daily_returns = daily_returns[1:]
+    cum_ret = (port_val[-1]/port_val[0] - 1)
+    avg_daily_ret = daily_returns.mean()
+    std_daily_ret = daily_returns.std() 
+
+    SR = m.sqrt(sf) * (avg_daily_ret - rfr) / std_daily_ret 
+
+    cr, adr, sddr, sr = [  		  	   		  		 			  		 			     			  	 
+        cum_ret,  		  	   		  		 			  		 			     			  	 
+        avg_daily_ret,  		  	   		  		 			  		 			     			  	 
+        std_daily_ret,  		  	   		  		 			  		 			     			  	 
+        SR,  		  	   		  		 			  		 			     			  	 
+    ] 
+
+    return cr, adr, sddr, sr
+
+def findCompareStats(sym, portval):
+    dates = pd.date_range(portval.first_valid_index(), portval.last_valid_index())  		  	   		  		 			  		 			     			  	 
+    prices_all = get_data(sym, dates)  # automatically adds SPY  	
+    prices_all.fillna(method="ffill", inplace=True)
+    prices_all.fillna(method="bfill", inplace=True)	
+    prices_all = prices_all.dropna()	  		 			     			  	 
+    prices = prices_all[sym]  # only portfolio symbols 
+
+    rv = np.sum(prices, axis=1)
+
+    return findPortStats(rv)
 
 def compute_portvals(  		  	   		  		 			  		 			     			  	 
-    orders_file="./orders/orders.csv",  		  	   		  		 			  		 			     			  	 
+    orders_file="./orders/orders-01.csv",  		  	   		  		 			  		 			     			  	 
     start_val=1000000,  		  	   		  		 			  		 			     			  	 
     commission=9.95,  		  	   		  		 			  		 			     			  	 
     impact=0.005,  		  	   		  		 			  		 			     			  	 
@@ -60,21 +96,67 @@ def compute_portvals(
     :type impact: float  		  	   		  		 			  		 			     			  	 
     :return: the result (portvals) as a single-column dataframe, containing the value of the portfolio for each trading day in the first column from start_date to end_date, inclusive.  		  	   		  		 			  		 			     			  	 
     :rtype: pandas.DataFrame  		  	   		  		 			  		 			     			  	 
-    """  		  	   		  		 			  		 			     			  	 
-    # this is the function the autograder will call to test your code  		  	   		  		 			  		 			     			  	 
-    # NOTE: orders_file may be a string, or it may be a file object. Your  		  	   		  		 			  		 			     			  	 
-    # code should work correctly with either input  		  	   		  		 			  		 			     			  	 
-    # TODO: Your code here  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
-    # In the template, instead of computing the value of the portfolio, we just  		  	   		  		 			  		 			     			  	 
-    # read in the value of IBM over 6 months  		  	   		  		 			  		 			     			  	 
-    start_date = dt.datetime(2008, 1, 1)  		  	   		  		 			  		 			     			  	 
-    end_date = dt.datetime(2008, 6, 1)  		  	   		  		 			  		 			     			  	 
-    portvals = get_data(["IBM"], pd.date_range(start_date, end_date))  		  	   		  		 			  		 			     			  	 
-    portvals = portvals[["IBM"]]  # remove SPY  		  	   		  		 			  		 			     			  	 
-    rv = pd.DataFrame(index=portvals.index, data=portvals.values)  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
-    return rv  		  	   		  		 			  		 			     			  	 
+    """  		  	   		  		 			  		 			     			  	 	  	   		  		 			  		 			     			  	 		 
+    
+    orders_df = pd.read_csv(orders_file, index_col='Date', parse_dates=True, na_values=['nan']) 
+    orders_df = orders_df.sort_values(by='Date',ascending=True)
+
+    sym = set()
+    for x in orders_df['Symbol']:
+        sym.add(x)
+    
+    dates = pd.date_range(orders_df.first_valid_index(), orders_df.last_valid_index())  		  	   		  		 			  		 			     			  	 
+    prices_all = get_data(sym, dates)  # automatically adds SPY  	
+    prices_all.fillna(method="ffill", inplace=True)
+    prices_all.fillna(method="bfill", inplace=True)	
+    prices_all = prices_all.dropna()	  		 			     			  	 
+    prices = prices_all[sym]  # only portfolio symbols 
+    prices['Cash'] = 1.0
+
+    trades = prices.copy()
+    for x in trades:
+        trades[x] = 0.0
+
+    for x in prices.index:
+        if x in orders_df.index:
+            symb = orders_df['Symbol'][x]
+            order = orders_df['Order'][x]
+            shares = orders_df['Shares'][x]
+            if type(order) != str:
+                for i in range(len(order)):
+                    if order[i] == 'SELL':
+                        trades[symb[i]][x] = trades[symb[i]][x] - float(shares[i])
+                        trades['Cash'][x] = trades['Cash'][x] + (float(shares[i]) * (prices[symb[i]][x] * (1.0-impact))) - float(commission)
+                    elif order[i] == 'BUY':
+                        trades[symb[i]][x] = trades[symb[i]][x] + float(shares[i])
+                        trades['Cash'][x] = trades['Cash'][x] - (float(shares[i]) * (prices[symb[i]][x] * (1.0+impact))) - float(commission)
+            else:
+                if order == 'SELL':
+                    trades[symb][x] = trades[symb][x] - float(shares)
+                    trades['Cash'][x] = trades['Cash'][x] + (float(shares) * (prices[symb][x] * (1.0-impact))) - float(commission)
+                elif order == 'BUY':
+                    trades[symb][x] = trades[symb][x] + float(shares)
+                    trades['Cash'][x] = trades['Cash'][x] - (float(shares) * (prices[symb][x] * (1.0+impact))) - float(commission)
+
+    yest = {}
+    for s in sym:
+        yest[s] = 0.0
+    yest['Cash'] = float(start_val)
+
+    holding = trades.copy()
+    for x in holding:
+        holding[x] = 0.0
+
+    for i in holding.index:
+        for s in sym:
+            holding[s][i] = trades[s][i] + yest[s]
+            yest[s] = trades[s][i] + yest[s]
+        holding['Cash'][i] = trades['Cash'][i] + yest['Cash']
+        yest['Cash'] = trades['Cash'][i] + yest['Cash']
+    
+    values = prices * holding
+    portvals = np.sum(values, axis=1)
+		 			  		 			     			  	 
     return portvals  		  	   		  		 			  		 			     			  	 
   		  	   		  		 			  		 			     			  	 
   		  	   		  		 			  		 			     			  	 
@@ -86,7 +168,7 @@ def test_code():
     # note that during autograding his function will not be called.  		  	   		  		 			  		 			     			  	 
     # Define input parameters  		  	   		  		 			  		 			     			  	 
   		  	   		  		 			  		 			     			  	 
-    of = "./orders/orders2.csv"  		  	   		  		 			  		 			     			  	 
+    of = "./orders/additional_orders/orders2.csv"  		  	   		  		 			  		 			     			  	 
     sv = 1000000  		  	   		  		 			  		 			     			  	 
   		  	   		  		 			  		 			     			  	 
     # Process orders  		  	   		  		 			  		 			     			  	 
@@ -97,36 +179,26 @@ def test_code():
         "warning, code did not return a DataFrame"  		  	   		  		 			  		 			     			  	 
   		  	   		  		 			  		 			     			  	 
     # Get portfolio stats  		  	   		  		 			  		 			     			  	 
-    # Here we just fake the data. you should use your code from previous assignments.  		  	   		  		 			  		 			     			  	 
-    start_date = dt.datetime(2008, 1, 1)  		  	   		  		 			  		 			     			  	 
-    end_date = dt.datetime(2008, 6, 1)  		  	   		  		 			  		 			     			  	 
-    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = [  		  	   		  		 			  		 			     			  	 
-        0.2,  		  	   		  		 			  		 			     			  	 
-        0.01,  		  	   		  		 			  		 			     			  	 
-        0.02,  		  	   		  		 			  		 			     			  	 
-        1.5,  		  	   		  		 			  		 			     			  	 
-    ]  		  	   		  		 			  		 			     			  	 
-    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = [  		  	   		  		 			  		 			     			  	 
-        0.2,  		  	   		  		 			  		 			     			  	 
-        0.01,  		  	   		  		 			  		 			     			  	 
-        0.02,  		  	   		  		 			  		 			     			  	 
-        1.5,  		  	   		  		 			  		 			     			  	 
-    ]  		  	   		  		 			  		 			     			  	 
-  		  	   		  		 			  		 			     			  	 
+    # Here we just fake the data. you should use your code from previous assignments.  		  	   		  		 			  		 			     			  	 		  	   		  		 			  		 			     			  	 
+    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = findPortStats(portvals)	
+
+    compare = ['$SPX']
+    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = findCompareStats(compare,portvals)
+
     # Compare portfolio against $SPX  		  	   		  		 			  		 			     			  	 
-    print(f"Date Range: {start_date} to {end_date}")  		  	   		  		 			  		 			     			  	 
+    print(f"Date Range: {portvals.first_valid_index()} to {portvals.last_valid_index()}")  		  	   		  		 			  		 			     			  	 
     print()  		  	   		  		 			  		 			     			  	 
     print(f"Sharpe Ratio of Fund: {sharpe_ratio}")  		  	   		  		 			  		 			     			  	 
-    print(f"Sharpe Ratio of SPY : {sharpe_ratio_SPY}")  		  	   		  		 			  		 			     			  	 
+    print(f"Sharpe Ratio of {compare[0]} : {sharpe_ratio_SPY}")  		  	   		  		 			  		 			     			  	 
     print()  		  	   		  		 			  		 			     			  	 
     print(f"Cumulative Return of Fund: {cum_ret}")  		  	   		  		 			  		 			     			  	 
-    print(f"Cumulative Return of SPY : {cum_ret_SPY}")  		  	   		  		 			  		 			     			  	 
+    print(f"Cumulative Return of {compare[0]} : {cum_ret_SPY}")  		  	   		  		 			  		 			     			  	 
     print()  		  	   		  		 			  		 			     			  	 
     print(f"Standard Deviation of Fund: {std_daily_ret}")  		  	   		  		 			  		 			     			  	 
-    print(f"Standard Deviation of SPY : {std_daily_ret_SPY}")  		  	   		  		 			  		 			     			  	 
+    print(f"Standard Deviation of {compare[0]} : {std_daily_ret_SPY}")  		  	   		  		 			  		 			     			  	 
     print()  		  	   		  		 			  		 			     			  	 
     print(f"Average Daily Return of Fund: {avg_daily_ret}")  		  	   		  		 			  		 			     			  	 
-    print(f"Average Daily Return of SPY : {avg_daily_ret_SPY}")  		  	   		  		 			  		 			     			  	 
+    print(f"Average Daily Return of {compare[0]} : {avg_daily_ret_SPY}")  		  	   		  		 			  		 			     			  	 
     print()  		  	   		  		 			  		 			     			  	 
     print(f"Final Portfolio Value: {portvals[-1]}")  		  	   		  		 			  		 			     			  	 
   		  	   		  		 			  		 			     			  	 
